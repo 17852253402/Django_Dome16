e226f081fbd6e48d932c463d7ca57bdbde877f64
mt_git <17852253402@163.com>
2019-03-28 16:43:12 +0800

    添加myfiter文件，修改了models

--- stuapp/models.py
+++ stuapp/models.py
@@ -38,8 +38,9 @@ class student(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
 
-class one(models.Model):
-    name = models.CharField(max_length=30)
 
 class two(models.Model):
+    name = models.CharField(max_length=30)
+
+class one(models.Model):
     name = models.CharField(max_length=30)
